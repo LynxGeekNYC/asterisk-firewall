d64b1d0592aa6fe6f34ff8ccc8f4d8093e5d9410
Alexander Mirvis <45396441+LynxGeekNYC@users.noreply.github.com>
2020-07-16 00:12:26 -0400

    Update firewall.stop.sh

--- firewall.stop.sh
+++ firewall.stop.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 IPTABLES=/sbin/iptables
-# Limpa todas as Rules em todas as tabelas
+# Clear all Rules on all tables
 $IPTABLES -t filter -F
 $IPTABLES -t nat -F
 $IPTABLES -t mangle -F
-# Remove todas as Chains em todas as tabelas
+# Removes all Chains in all tables
 $IPTABLES -t filter -X
 $IPTABLES -t nat -X
 $IPTABLES -t mangle -X
-# Zera todos os contadores de todas as Chains em todas as tabelas
+# Reset all counters for all Chains in all tables
 $IPTABLES -t filter -Z
 $IPTABLES -t nat -Z
 $IPTABLES -t mangle -Z
-# Aceita tudo por default (policy = ACCEPT)
+# Accept everything by default (policy = ACCEPT)
 $IPTABLES -t filter -P INPUT ACCEPT
 $IPTABLES -t filter -P FORWARD ACCEPT
 $IPTABLES -t filter -P OUTPUT ACCEPT
